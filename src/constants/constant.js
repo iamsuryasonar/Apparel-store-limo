3365c4536cc63df339c634a6919f90f1a0d102e5
Surya Sonar <iamsuryasonar@gmail.com>
2024-02-08 01:47:23 +0530

    fix: baseURL added

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'Limo';
 export const LOCAL_STORAGE_NAME = 'user';
-export const baseUrl = 'http://localhost:3001';
-// const baseUrl = import.meta.env.VITE_BASE_URL;
+// export const baseUrl = 'http://localhost:3001';
+const baseUrl = import.meta.env.VITE_BASE_URL;
 export const API_URL = baseUrl + '/api/v1/'
